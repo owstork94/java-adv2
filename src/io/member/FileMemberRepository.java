456e82da3c17a30c32ab250b42fdc2d652d9bb17
owsto <owstork94@gmail.com>
2024-12-14 13:07:43 +0900

    241213_1

--- src/io/member/FileMemberRepository.java
+++ src/io/member/FileMemberRepository.java
@@ -1,13 +1,27 @@
 package io.member;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 public class FileMemberRepository implements MemberRepository{
 
+    private static final String FILE_PATH = "temp/members-txt.dat";
+    private static final String DELIMITER = ",";
 
     @Override
     public void add(Member member) {
-
+    try(
+            BufferedWriter bw = new BufferedWriter(new FileWriter(FILE_PATH,StandardCharsets.UTF_8, true));
+            )
+    {
+        bw.write(member.getId()+DELIMITER+member.getName()+DELIMITER+member.getAge());
+        bw.newLine();
+    } catch (IOException e) {
+        throw new RuntimeException(e);
+    }
     }
 
     @Override
